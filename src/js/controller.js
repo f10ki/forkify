ceae82de5af57cc0ecc3a64e44461504ca0df9c1
f10ki <109439430+f10ki@users.noreply.github.com>
2024-01-29 17:57:34 +0600

    First load of all project files

---
+++ src/js/controller.js
@@ -0,0 +1,33 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
+    recipeView.renderSpiner();
+
+    // 1 Loadind recipe
+    await model.loadRecipe(id);
+
+    // 2 Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
