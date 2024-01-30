1d06395ab957bb589df68562169b93a0ec6b7a1b
f10ki <109439430+f10ki@users.noreply.github.com>
2024-01-30 22:48:59 +0600

    Add pagination in search result

--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,7 @@
 import { async } from 'regenerator-runtime';
 import { API_URL } from './config.js';
+import { RES_PER_PAGE } from './config.js';
+
 import { getJSON } from './helpers.js';
 
 export const state = {
@@ -7,6 +9,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -49,4 +53,11 @@ export const loadSearchResults = async function (query) {
     console.error(`${err} 💔💔💔`);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage; // 0
+  const end = page * state.search.resultsPerPage; // 10
+  return state.search.results.slice(start, end);
 };
